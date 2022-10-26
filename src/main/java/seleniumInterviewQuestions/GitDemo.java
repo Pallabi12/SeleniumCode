fd529038b25684085d0d36588fffd173a0f7560f
Pallabi Bhar <pallabibhar.90@gmail.com>
2022-10-26 18:47:01 +0530

    added GitDemo class

---
+++ src/main/java/seleniumInterviewQuestions/GitDemo.java
@@ -0,0 +1,11 @@
+package seleniumInterviewQuestions;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This class is created to learn the git branching");
+	}
+
+}
